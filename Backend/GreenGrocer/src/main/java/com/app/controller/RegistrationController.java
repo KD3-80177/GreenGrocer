240a974d346775f48e4792d0dbc2f648ec1a2e51
Niraj Shaha <nirajshahatakri@gmail.com>
2024-02-06 11:58:07 +0530

    Controller Code Correct

--- Backend/GreenGrocer/src/main/java/com/app/controller/RegistrationController.java
+++ Backend/GreenGrocer/src/main/java/com/app/controller/RegistrationController.java
@@ -14,36 +14,13 @@ import com.app.entities.UserRegistration;
 import com.app.service.RegisterInterface;
 
 @RestController
-<<<<<<< HEAD
 @RequestMapping("/Registration")
 @CrossOrigin
-=======
-<<<<<<< HEAD
-@RequestMapping("/Registration")
-@CrossOrigin("http://localhost:3000")
-=======
-<<<<<<< HEAD
-@RequestMapping("/registration")
-@CrossOrigin(origins = "http://localhost:3000")
-=======
-@RequestMapping("/Registration")
-@CrossOrigin
->>>>>>> 8e4cce9479983da347addaedb3ba65e396c2b3d2
->>>>>>> 7479d3292a2e6efb0a6a7f4291fe3cf3b42df130
->>>>>>> 6d6bfc199de7571f17583a56ea63263648480d35
 public class RegistrationController {
 	
 	@Autowired
 	private RegisterInterface register;
 	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	@PostMapping("/newUser")
-=======
-<<<<<<< HEAD
-	
->>>>>>> 6d6bfc199de7571f17583a56ea63263648480d35
 	@GetMapping("/getUser/{id}")
 	public UserRegistration getUserInfo(@PathVariable Long id)
 	{
@@ -51,11 +28,6 @@ public class RegistrationController {
 	}
 	
 	@PostMapping("/newUser")
-<<<<<<< HEAD
-=======
->>>>>>> 8e4cce9479983da347addaedb3ba65e396c2b3d2
->>>>>>> 7479d3292a2e6efb0a6a7f4291fe3cf3b42df130
->>>>>>> 6d6bfc199de7571f17583a56ea63263648480d35
 	public String addUser(@RequestBody UserRegistration user) {
 		return register.addUser(user);
 	}
