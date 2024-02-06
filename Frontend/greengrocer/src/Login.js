8b3789d14e48dcd122bf8233960ce0b5ba621e39
kd3-80219 <vinayakshipekar19@gmail.com>
2024-02-06 15:35:31 +0530

    Login Changes

--- Frontend/greengrocer/src/Login.js
+++ Frontend/greengrocer/src/Login.js
@@ -1,12 +1,24 @@
 import { useState,useEffect } from "react";
 import { Link,useNavigate,useParams } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
 
     const url = "http://localhost:8080/user/login";
     const [email,setEMail] = useState('');
-    const [passord,setPassword] = useState('');
-    
+    const [pass,setPass] = useState('');
+
+    const CheckLogin = ()=>
+    {
+        const getUrl = url + "/" + email + "/" + pass;
+        axios.get(getUrl).then((result)=>{
+            var validUser = result.data;
+            if(validUser !== null){
+                console.log("Found");
+                setTimeout(5000);
+            }
+        });
+    }
     return (
         <div className="container">
             <h3>
@@ -27,13 +39,13 @@ function Login() {
                     <input
                     type="password"
                     className="form-control col-4"
-                    name="password"
-                    onChange={(e)=>setPassword(e.target.value)}
+                    name="pass"
+                    onChange={(e)=>setPass(e.target.value)}
                     placeholder="Enter Password Here"></input>
                 </div>
                 <div>
                     <button
-                    className="btn btn-primary" onClick={Login}>Login</button>
+                    className="btn btn-primary" onClick={CheckLogin}>Login</button>
                 </div>
             </form>
         </div>
