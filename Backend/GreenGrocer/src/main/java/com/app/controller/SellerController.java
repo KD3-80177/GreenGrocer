c7279f6b155bdca5ab876339b64be72e78874d97
kd3-80219 <vinayakshipekar19@gmail.com>
2024-02-07 14:49:11 +0530

    Seller Backend

--- Backend/GreenGrocer/src/main/java/com/app/controller/SellerController.java
+++ Backend/GreenGrocer/src/main/java/com/app/controller/SellerController.java
@@ -1,6 +1,32 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.entities.Seller;
+import com.app.service.SellerInterface;
+
+@RestController
+@RequestMapping("/seller")
+@CrossOrigin(origins="*",allowedHeaders = "*")
 public class SellerController {
 
+	@Autowired
+	private SellerInterface sellerService;
 	
+	@PostMapping("/newSeller")
+	public String addSeller(@RequestBody Seller newSeller)
+	{
+		return sellerService.addNewSeller(newSeller);
+	}
+	
+	@PostMapping("/login")
+	public Seller getSellerByEmail(@RequestBody Seller findSeller)
+	{
+		return sellerService.findSellerByEmail(findSeller);
+	}
 }
--- Backend/GreenGrocer/src/main/java/com/app/service/SellerImpl.java
+++ Backend/GreenGrocer/src/main/java/com/app/service/SellerImpl.java
@@ -1,10 +1,42 @@
 package com.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.app.dao.SellerDao;
+import com.app.entities.Seller;
 
 @Service
 @Transactional
 public class SellerImpl implements SellerInterface{
 
+	@Autowired
+	private SellerDao sellerDao;
+	
+	
+	
+	@Override
+	public String addNewSeller(Seller seller) {
+		sellerDao.save(seller);
+		return "Seller Added Successfully";
+	}
+	
+	@Override
+	public Seller findSellerByEmail(Seller findSeller) {
+		String email = findSeller.getEmail();
+		String password =  findSeller.getPassword();
+		
+		Seller seller = sellerDao.findSellerByEmail(email);
+		
+		if(seller!=null)
+		{
+			if(seller.getPassword()==password)
+			{
+				return seller;
+			}
+		}
+		return seller;
+	}
+	
 }
