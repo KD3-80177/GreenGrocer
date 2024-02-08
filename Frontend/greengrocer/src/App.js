6b46a3fd00582d5b181afd0852011d9a26626f75
KD3-80142 <ruturajmane1999@gmail.com>
2024-02-08 19:32:15 +0530

    Login frontend

--- Frontend/greengrocer/src/App.js
+++ Frontend/greengrocer/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import RegisterUser from './RegisterUser';
-import Login from './Login';
+import UserRegistration from './UserRegistration';
+import UserLogin from './UserLogin';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Routes>
-          { <Route exact path = '/Login' element={<Login/>}/>
+          { <Route exact path = '/Login' element={<UserLogin/>}/>
           /*<Route exact path = '/login' element={<Login/>}/> */}
-          <Route exact path = '/Registration' element={<RegisterUser/>}/>
+          <Route exact path = '/Registration' element={<UserRegistration/>}/>
           {/* <Route exact path = '*' element={<Login/>}/> */}
         </Routes>
       </div>
