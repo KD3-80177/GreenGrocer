c119d407a18bb42b9e4ca83d87dbd1f959c66b35
KD3-80142 <ruturajmane1999@gmail.com>
2024-02-09 17:25:55 +0530

    ForgetPassword Front End

--- Frontend/greengrocer/src/SellerLogin.js
+++ Frontend/greengrocer/src/SellerLogin.js
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
-import { Toast } from "bootstrap";
+
 function SellerLogin()
 {
     const url = "http://localhost:8080/seller/login";
