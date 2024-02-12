7621a07c9dd83fa113cac6e2b6153631fa242195
kd3-80219 <vinayakshipekar19@gmail.com>
2024-02-12 11:12:17 +0530

    Seller-User Registration UI Changed

--- Frontend/greengrocer/src/UserRegistration.js
+++ Frontend/greengrocer/src/UserRegistration.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 //import GreenGrocer from '../Services/greengrocer.service';
 import axios from "axios";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import {Form  ,Item,Input,BreadcrumbItemPropsnput,message, Typography, Divider} from "antd"; 
+import "./style.css";
 
 function UserRegistration(){
     
@@ -32,15 +34,15 @@ function UserRegistration(){
         .then((result)=>{
             if(result.data.affectedRows !== undefined && result.data.affectedRows>0){
                 showMessage("Customer Added Successfully!!!!!");
-                Reset();
             }
+            Reset();
         });
         
     }
 
 return(
     <center>
-        <h1><strong>Registration</strong></h1>
+        {/* <h1><strong>Registration</strong></h1>
     <div className="container">
     <div className="jumbotron">
             
@@ -135,6 +137,102 @@ return(
                 </tbody>
             </table>
         </div>
+    </div>
+    </div> */}
+     <div className="userRegBg">
+    <div className="container">
+       
+
+<Form className="registerForm" >
+              <Typography.Title style={{fontFamily:"sans-serif",textAlign:"center",color:"green",marginTop:"20px"}}>USER REGISTRATION</Typography.Title>
+              <Divider style={{borderColor:'green'}}></Divider>
+
+              <Form.Item rules={[
+                {
+                  required:true,
+                },
+                   ]}label="FullName" name={'fullName'}>
+                <Input  required placeholder="Enter your Full Name"  name="fullName"
+                            value={user.fullName}
+                            onChange={OnTextChange} />
+              </Form.Item>
+              <Form.Item rules={[
+                {
+                  required:true,
+                },
+                   ]}label="mobileNo" name={'mobileNo'}>
+                <Input  required placeholder="Enter your mobileNo" name="mobileNo"
+                            value={user.mobileNo}
+                            onChange={OnTextChange} />
+              </Form.Item>
+             
+              <Form.Item 
+              rules={[
+                {
+                  required:true,
+                  type:"email",
+                  message:"Please Enter Valid Email!!"
+                },
+                  ]} label="Email" name={'email'}>
+                <Input required placeholder="Enter your email"  name="email"
+                            value={user.email}
+                            onChange={OnTextChange}/>
+              </Form.Item>
+              <Form.Item rules={[
+                {
+                  required:true,
+                  message:"Please Enter Valid Password!!"
+                },
+                   ]}label="Password" name={'password'}>
+                <Input.Password  required placeholder="Enter your password"  name="password"
+                            value={user.password}
+                            onChange={OnTextChange} />
+              </Form.Item>
+              <Form.Item rules={[
+                {
+                  required:true,
+                },
+                   ]}label="Address" name={'address'}>
+                <Input  required placeholder="Enter your address"  name="address"
+                            value={user.address}
+                            onChange={OnTextChange} />
+              </Form.Item>
+              <Form.Item rules={[
+                {
+                  required:true,
+                },
+                   ]}label="city" name={'city'}>
+                <Input  required placeholder="Enter your city" name="city"
+                            value={user.city}
+                            onChange={OnTextChange} />
+              </Form.Item>
+              <Form.Item rules={[
+                {
+                  required:true,
+                },
+                   ]}label="state" name={'state'}>
+                <Input  required placeholder="Enter your state" name="state"
+                            value={user.state}
+                            onChange={OnTextChange}/>
+              </Form.Item>
+              <Form.Item rules={[
+                {
+                  required:true,
+                },
+                   ]}label="Pincode" name={'pinCode'}>
+                <Input required placeholder="Enter your pinCode" name="pinCode"
+                               value={user.pinCode}
+                            onChange={OnTextChange} />
+              </Form.Item>
+              <button className="btn btn-primary"  style={{marginLeft:"100px",display:"block"}} onClick={saveUser}>
+                REGISTER
+              </button>
+              <input type="reset"  className="btn btn-success" value={"Reset"} onClick={Reset}/>
+
+              
+              <Divider style={{borderColor:'green'}}></Divider>
+              
+            </Form>
     </div>
     </div>
     </center>
