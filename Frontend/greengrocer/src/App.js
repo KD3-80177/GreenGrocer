48777acbd9b03f98c06393a373187976548d5beb
Shivshankar Patil <patilshivshankar99@gmail.com>
2024-02-16 11:41:21 +0530

    working on seller dashboard

--- Frontend/greengrocer/src/App.js
+++ Frontend/greengrocer/src/App.js
@@ -18,9 +18,9 @@ import UpdateUser from './UpdateUser';
 import AllUser from './AllUser';
 import AllProducts from './AllProducts';
 import AllDelieryBoy from './AllDeliveryBoy';
-
+import SellerDashboard from './SellerDashboard';
 import Products from './Products';
-
+import AllOrder from './AllOrder';
 
 function App() {
   return (
@@ -36,8 +36,7 @@ function App() {
           <Route exact path = '/SellerLogin' element={<SellerLogin/>}/>
           <Route exact path = '/Forget' element={<ResetPassword/>}/>
           <Route exact path = '/AdminDashboard' element={<AdminDashboard/>}/>
-          <Route exact path='/DeliveryBoy' element={<DeliveryBoyDash/>}/>
-          
+          <Route exact path = '/SellerDashboard' element={<SellerDashboard/>}/>
  
           {/* <Route exact path = '*' element={<Login/>}/> */}
           {<Route exact path = '/Update' element={<UpdateUser/>}/>}
@@ -50,6 +49,11 @@ function App() {
           <Route exact path = '/getAllProducts' Component={AllProducts}/>
           <Route exact path = '/getAllDeliveryBoys' Component={AllDelieryBoy}/>
           <Route exact path = '/goToAdminDashboard' Component={AdminDashboard}/>
+
+
+          {/* Seller Dashboard */}
+          <Route exact path = '/getAllOrders' Component={AllOrder}/>
+
           {/* <Route exact path = '*' element={<Login/>}/> */}
           <Route exact path = '/Update' element={<UpdateUser/>}/>
           <Route exact path = '/products' element={<Products/>}/>
---
+++ Frontend/greengrocer/src/SellerSidebar.js
@@ -0,0 +1,51 @@
+import {BsCart3, BsFillArchiveFill, BsGrid1X2Fill, BsMenuButtonWideFill, BsPeopleFill} from 'react-icons/bs'
+import './AdminStyle.css'
+import { Link } from 'react-router-dom'
+
+function SellerSidebar({openSidebarToggle, OpenSidebar})
+{
+    return(
+        <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive":""}>
+        <div className='sidebar-title'>
+            <div className='sidebar-brand'>
+                <BsCart3 className='icon_header'/> GreenGrocer
+            </div>
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+        </div>
+
+            <ul className='sidebar-list'>
+                <li className='sidebar-list-item'>
+                    <Link to={'/goToAdminDashboard'}>
+                    <BsGrid1X2Fill className='icon'/>Seller Dashboard
+                    </Link>
+                    
+                </li>
+                <li className='sidebar-list-item'>
+                    <Link to={'/getAllProducts'}>
+                    <BsFillArchiveFill className='icon'/>Add Delivery Boy
+                    </Link>
+                </li>
+                <li className='sidebar-list-item'>
+                    <Link to={'/getAllOrders'}>
+                        <BsPeopleFill className='icon'/>View orders
+                    </Link>
+                </li>
+                <li className='sidebar-list-item'>
+                    <Link to={'/getAllSellers'}><BsPeopleFill className='icon'/>Update Profile
+                    </Link>
+                </li>
+                <li className='sidebar-list-item'>
+                    <Link to={'/getAllDeliveryBoys'}>
+                        <BsPeopleFill className='icon'/>Delivery Boys
+                    </Link>
+                </li>
+                <li className='sidebar-list-item'>
+                    <a href="">
+                        <BsMenuButtonWideFill className='icon'/>Logout
+                    </a>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+export default SellerSidebar
